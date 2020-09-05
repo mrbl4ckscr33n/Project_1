4d31dc97022f78daa546c8dd28473e470dc4c26f
LetsCrashGerman <70725566+LetsCrashGerman@users.noreply.github.com>
2020-09-06 01:56:09 +0200

    Add files via upload

---
+++ Movement.hx
@@ -0,0 +1,51 @@
+package arm;
+
+import iron.Trait;
+import iron.system.Input;
+import iron.math.Vec4;
+import iron.object.Transform;
+import armory.trait.physics.RigidBody;
+import armory.trait.physics.PhysicsWorld;
+
+
+class Movement extends Trait
+{
+	var body: RigidBody;
+	var transform_movement:Transform;
+	var rotationSpeed = 1;
+	var speed = 0.1;
+		public function new() 
+	{
+		super();
+		iron.Scene.active.notifyOnInit(function () {
+			transform_movement = object.transform;
+			body = object.getTrait(RigidBody);
+			PhysicsWorld.active.notifyOnPreUpdate(mouselock);
+		});
+
+		notifyOnUpdate (walking);
+	}
+
+	function mouselock()
+	{
+		if (Input.occupied || !body.ready) return;
+
+		if (Input.getMouse().started() && !Input.getMouse().locked) Input.getMouse().lock();
+		else if (Input.getKeyboard().started("escape") && Input.getMouse().locked) Input.getMouse().unlock();
+
+		if (Input.getMouse().moved) transform_movement.rotate(Vec4.zAxis(),-Input.getMouse().movementX / 250 * rotationSpeed);
+		body.syncTransform();
+	}
+
+	function walking()
+	{
+		if (Input.getKeyboard().down("shift")) speed = 0.15;
+		else speed = 0.1;
+
+		if (Input.getKeyboard().down("w")) transform_movement.move(object.transform.look(),speed);
+		if (Input.getKeyboard().down("s")) transform_movement.move(object.transform.look().mult(-1),speed);
+		if (Input.getKeyboard().down("d")) transform_movement.move(object.transform.right(),speed);
+		if (Input.getKeyboard().down("a")) transform_movement.move(object.transform.right().mult(-1),speed); 
+		body.setAngularFactor(0, 0, 0);
+	}
+}
