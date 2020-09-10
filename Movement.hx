82ca63f7119d9bf1deab7299089212fe39fa4a3f
mrbl4ckscr33n <70725646+mrbl4ckscr33n@users.noreply.github.com>
2020-09-10 03:15:26 +0200

    Add files via upload

--- Movement.hx
+++ Movement.hx
@@ -55,28 +55,25 @@ class Movement extends Trait
 			if (Input.getKeyboard().down("w"))
 			{
 				current_dir = object.transform.look();
-				transform_movement.move(object.transform.look(), speed);
 			}
 			else if (Input.getKeyboard().down("s"))
 			{
 				current_dir = current_dir.addvecs(current_dir, object.transform.look().mult(-1));
-				transform_movement.move(object.transform.look().mult(-1), speed);
 			}
 			if (Input.getKeyboard().down("d"))
 			{
 				current_dir = current_dir.addvecs(current_dir, object.transform.right());
-				transform_movement.move(object.transform.right(), speed);
 			}
 			else if (Input.getKeyboard().down("a"))
 			{
 				current_dir = current_dir.addvecs(current_dir, object.transform.right().mult(-1));
-				transform_movement.move(object.transform.right().mult(-1), speed);
 			}
+			transform_movement.move(current_dir.normalize(), speed);
 		}
 
 		else
 		{
-			transform_movement.move(current_dir, speed);
+			transform_movement.move(current_dir.normalize(), speed);
 		}
 		
 		body.setAngularFactor(0, 0, 0);
