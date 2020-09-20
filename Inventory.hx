fd3397a174d493a817cc9b70dd7914b1160f41ff
mrbl4ckscr33n <70725646+mrbl4ckscr33n@users.noreply.github.com>
2020-09-20 21:44:25 +0200

    Add files via upload

--- Inventory.hx
+++ Inventory.hx
@@ -2,6 +2,7 @@ package arm;
 
 import iron.Scene;
 import iron.App;
+import iron.math.Vec4;
 import iron.system.Input;
 import armory.system.Event;
 import armory.trait.internal.CanvasScript;
@@ -10,12 +11,17 @@ import armory.trait.physics.bullet.PhysicsWorld;
 
 class Inventory extends Sight
 {
+	var canvas: CanvasScript;
+	var inventoryOpen: Bool = false;
+	var hit_1: Hit;
+	var impili: Vec4 = new Vec4(0.0);
+	var pickedUp: Bool = false;
+
 	public function new()
 	{
 		super();
 
-		var canvas: CanvasScript;
-		var inventoryOpen: Bool = false;
+		impili.z = 10;
 
 		notifyOnInit(function()
 		{
@@ -31,9 +37,9 @@ class Inventory extends Sight
 		});
 
 		function toggleInventory()
-		{
+		{	
 			if (Input.occupied) return;
-
+			
 				if(Input.getMouse().locked)
 				{
 					canvas.setCanvasVisibility(true);
@@ -57,9 +63,37 @@ class Inventory extends Sight
 			else if (Input.getKeyboard().started("escape") && Input.getMouse().locked) Input.getMouse().unlock();
 		}
 
+		function pickUp()
+		{
+			if(Input.getKeyboard().started("f"))
+			{
+				var currentDir = new Vec4(0.0);
+				var currentLoc = new Vec4(0.0);
+
+				currentLoc.x = object.transform.worldx();
+				currentLoc.y = object.transform.worldy();
+				currentLoc.z = object.transform.worldz();
+			
+				// get look vector (3m long):
+				currentDir = object.transform.up();
+				currentDir.mult(-3);
+
+				hit_1 = PhysicsWorld.active.rayCast(currentLoc, currentDir.add(currentLoc));
+				pickedUp = true;
+			}
+
+			if((hit_1 != null) && hit_1.rb.ready && pickedUp)
+			{
+				hit_1.rb.applyImpulse(impili);
+				pickedUp = false;
+			}
+
+		}
+
 		notifyOnUpdate(function()
 		{
 			if(Input.getKeyboard().started("e")) toggleInventory();
+			pickUp();
 			lockMouse();
 		});
 
@@ -70,3 +104,23 @@ class Inventory extends Sight
 	}
 }
 
+class Item
+{
+	public var onGround: Bool = true;
+	public var amountInventory: Int = 0;
+	public var ready: Bool = false;
+
+	public function new()
+	{
+		ready = true;
+		//iron.Scene.active.spawnObject();
+	}
+}
+
+class WeirdObject extends Item
+{
+	public function new()
+	{
+		super();
+	}
+}
