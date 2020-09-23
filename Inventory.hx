74218eae372fe8601b19fa68ce3152bf21c27ee2
mrbl4ckscr33n <70725646+mrbl4ckscr33n@users.noreply.github.com>
2020-09-23 04:21:49 +0200

    Add files via upload

--- Inventory.hx
+++ Inventory.hx
@@ -18,108 +18,18 @@ class Inventory extends Sight
 	var inventoryOpen: Bool = false;
 	var pickUpHit: Hit;
 	var pickedUp: Bool = false;
+	var itemContainer = new Array<RigidBody>();
 
 	public function new()
 	{
 		super();
 
-		var itemContainer = new Array<RigidBody>();
-
 		notifyOnInit(function()
 		{
 			canvas = object.getTrait(CanvasScript);
+			canvas.setCanvasVisibility(false);
+			canvas.setUiScale(0.5);
 		});
-
-		function toggleInventory()
-		{	
-			if (Input.occupied) return;
-			
-				if(Input.getMouse().locked)
-				{
-					canvas.setCanvasVisibility(true);
-					inventoryOpen = true;
-					Input.getMouse().unlock(); // unlock mouse while in inventory
-				}
-
-				else if(!Input.getMouse().locked)
-				{
-					canvas.setCanvasVisibility(false);
-					inventoryOpen = false;
-					Input.getMouse().lock(); // lock mouse when inventory is closed
-				}
-		};
-
-		function lockMouse()
-		{
-			if (Input.occupied) return;
-		
-			if (Input.getMouse().started() && !Input.getMouse().locked && !inventoryOpen) Input.getMouse().lock();
-			else if (Input.getKeyboard().started("escape") && Input.getMouse().locked) Input.getMouse().unlock();
-		}
-
-		function pickUp()
-		{
-			if(Input.getKeyboard().started("f"))
-			{
-				var currentDir = new Vec4(0.0);
-				var currentLoc = new Vec4(0.0);
-
-				currentLoc.x = object.transform.worldx();
-				currentLoc.y = object.transform.worldy();
-				currentLoc.z = object.transform.worldz();
-			
-				// get look vector (3m long):
-				currentDir = object.transform.up();
-				currentDir.mult(-3);
-
-				pickUpHit = PhysicsWorld.active.rayCast(currentLoc, currentDir.add(currentLoc));
-				pickedUp = true;
-			}
-
-			if((pickUpHit != null) && pickUpHit.rb.ready && pickedUp)
-			{
-				if(pickUpHit.rb.object.properties != null)
-				{
-					if(pickUpHit.rb.object.properties["pickUpAble"])
-					{
-						itemContainer.push(pickUpHit.rb);
-						pickUpHit.rb.object.remove();
-						trace(itemContainer[0]);
-					}
-				}
-				pickedUp = false;
-			}
-		}
-
-		function placeDown()
-		{
-			if (Input.getKeyboard().started("i") && (itemContainer.length > 0))
-				{
-					var drop = itemContainer.pop();
-
-					var currentDir = new Vec4(0.0);
-					var currentLoc = new Vec4(0.0);
-
-					currentLoc.x = object.transform.worldx();
-					currentLoc.y = object.transform.worldy();
-					currentLoc.z = object.transform.worldz();
-
-					// get look vector (3m long):
-					currentDir = object.transform.up();
-					currentDir.mult(-3);
-
-					currentDir.add(currentLoc);
-
-					iron.Scene.active.spawnObject(drop.object.name, null, function(o:Object)
-					{
-						o.transform.loc = currentDir.add(new Vec4(0,0,1));
-						o.transform.buildMatrix();
-						
-						var dropped = o.getTrait(RigidBody);
-						dropped.syncTransform();
-					});
-				}
-		}
 
 		notifyOnUpdate(function()
 		{
@@ -134,32 +44,95 @@ class Inventory extends Sight
 		// notifyOnRemove(function() {
 		// });
 	}
-}
 
-class Item
-{
-	public var amount: Int;
-	public var ready: Bool = false;
+	function toggleInventory()
+	{	
+		if (Input.occupied) return;
+			
+		if(Input.getMouse().locked)
+		{
+			canvas.setCanvasVisibility(true);
+			inventoryOpen = true;
+			Input.getMouse().unlock(); // unlock mouse while in inventory
+		}
 
-	public function new()
+		else if(!Input.getMouse().locked)
+		{
+			canvas.setCanvasVisibility(false);
+			inventoryOpen = false;
+			Input.getMouse().lock(); // lock mouse when inventory is closed
+		}
+	};
+
+	function lockMouse()
 	{
-		amount = 0;
-
-		ready = true;
+		if (Input.occupied) return;
+			
+		if (Input.getMouse().started() && !Input.getMouse().locked && !inventoryOpen) Input.getMouse().lock();
+		else if (Input.getKeyboard().started("escape") && Input.getMouse().locked) Input.getMouse().unlock();
 	}
 
-	function updateItem()
+	function pickUp()
 	{
+		if(Input.getKeyboard().started("f"))
+		{
+			var currentDir = new Vec4(0.0);
+			var currentLoc = new Vec4(0.0);
 
+			currentLoc.x = object.transform.worldx();
+			currentLoc.y = object.transform.worldy();
+			currentLoc.z = object.transform.worldz();
+			
+			// get look vector (3m long):
+			currentDir = object.transform.up();
+			currentDir.mult(-3);
+
+			pickUpHit = PhysicsWorld.active.rayCast(currentLoc, currentDir.add(currentLoc));
+			pickedUp = true;
+		}
+
+		if((pickUpHit != null) && pickUpHit.rb.ready && pickedUp)
+		{
+			if(pickUpHit.rb.object.properties != null)
+			{
+				if(pickUpHit.rb.object.properties["pickUpAble"])
+				{
+					itemContainer.push(pickUpHit.rb);
+					pickUpHit.rb.object.remove();
+					trace(itemContainer[0]);
+				}
+			}
+			pickedUp = false;
+		}
 	}
-}
 
-class TreeItem extends Item
-{
-	public var treeColor: String;
-	public function new()
+	function placeDown()
 	{
-		treeColor = "green";
-		super();
+		if (Input.getKeyboard().started("i") && (itemContainer.length > 0))
+		{
+			var drop = itemContainer.pop();
+	
+			var currentDir = new Vec4(0.0);
+			var currentLoc = new Vec4(0.0);
+	
+			currentLoc.x = object.transform.worldx();
+			currentLoc.y = object.transform.worldy();
+			currentLoc.z = object.transform.worldz();
+	
+			// get look vector (3m long):
+			currentDir = object.transform.up();
+			currentDir.mult(-3);
+	
+			currentDir.add(currentLoc);
+	
+			iron.Scene.active.spawnObject(drop.object.name, null, function(o:Object)
+			{
+				o.transform.loc = currentDir.add(new Vec4(0,0,1));
+				o.transform.buildMatrix();
+							
+				var dropped = o.getTrait(RigidBody);
+				dropped.syncTransform();
+			});
+		}
 	}
 }
