b1a45cef2cfba9e468a60fcfb4738fc8f88fa0ff
mrbl4ckscr33n <70725646+mrbl4ckscr33n@users.noreply.github.com>
2020-10-11 21:31:30 +0200

    Add files via upload

--- Inventory.hx
+++ Inventory.hx
@@ -1,7 +1,6 @@
 package arm;
 
 import armory.system.Event;
-import armory.trait.internal.CanvasScript;
 import armory.trait.physics.bullet.PhysicsWorld;
 import armory.trait.physics.bullet.RigidBody;
 import iron.App;
@@ -11,32 +10,29 @@ import iron.math.Vec4;
 import iron.object.Object;
 import iron.system.Input;
 import iron.data.Data;
+import zui.NewCanvasScript;
+import zui.ItemOps;
 
 class Inventory extends Sight
 {
-	var canvas: CanvasScript;
-
 	var inventoryOpen: Bool = false;
 	var pickUpHit: Hit;
 	var pickedUp: Bool = false;
-	var itemContainer = new Array<RigidBody>();
+	//var itemContainer = new Array<RigidBody>();
+	var ncs_1: NewCanvasScript;
+	var background_1: Telement = {id: null, type: Container, name: null, x: null, y: null, width: 100, height: 100, alignment: 5, visible: false};
+	var asset_1: Tasset = {id: null, file: "../../Assets/inventory_background.png", name: null};
 
 	public function new()
 	{
 		super();
 
-		//var rawObject: TObj;
-		//var format: TSceneFormat;
-
-		//iron.Scene.active.createObject();
+		ncs_1 = new NewCanvasScript();
+		ItemOps.pushElements(ncs_1, background_1, asset_1);
 
 		notifyOnInit(function()
 		{
-			canvas = object.getTrait(CanvasScript);
-			canvas.setCanvasVisibility(false);
-			canvas.setUiScale(0.5);
-			/*var file = "icosphere.png";
-			iron.data.Data.getImage(file, function(image: kha.Image) {trace(image);});*/
+
 		});
 
 		notifyOnUpdate(function()
@@ -59,14 +55,14 @@ class Inventory extends Sight
 			
 		if(Input.getMouse().locked)
 		{
-			canvas.setCanvasVisibility(true);
+			ncs_1.setCanvasVisibility(true);
 			inventoryOpen = true;
 			Input.getMouse().unlock(); // unlock mouse while in inventory
 		}
 
 		else if(!Input.getMouse().locked)
 		{
-			canvas.setCanvasVisibility(false);
+			ncs_1.setCanvasVisibility(false);
 			inventoryOpen = false;
 			Input.getMouse().lock(); // lock mouse when inventory is closed
 		}
@@ -107,9 +103,9 @@ class Inventory extends Sight
 			{
 				if(pickUpHit.rb.object.properties["pickUpAble"])
 				{
-					itemContainer.push(pickUpHit.rb);
+					//itemContainer.push(pickUpHit.rb);
 					pickUpHit.rb.object.remove();
-					trace(itemContainer[0]);
+					ItemOps.pushItems(ncs_1, pickUpHit.rb);
 				}
 			}
 			pickedUp = false;
@@ -118,7 +114,7 @@ class Inventory extends Sight
 
 	function placeDown()
 	{
-		if (Input.getKeyboard().started("q") && (itemContainer.length > 0))
+		/*if (Input.getKeyboard().started("q") && (itemContainer.length > 0))
 		{
 			var drop = itemContainer.pop();
 	
@@ -140,7 +136,56 @@ class Inventory extends Sight
 				o.transform.loc = currentDir.add(new Vec4(0,0,1));
 				o.transform.buildMatrix();
 
-				o.properties = drop.object.properties;
+				//o.properties = drop.object.properties;
+							
+				var dropped = o.getTrait(RigidBody);
+				dropped.syncTransform();
+			});
+		}*/
+
+		if (Input.getKeyboard().started("q"))
+		{
+			//find first DragAble in canvas:
+			var nameToDrop: String = null;
+			if(ncs_1.canvas_1.elements != null)
+			{
+				var a: Int = ncs_1.canvas_1.elements.length - 1;
+				while(a >= 0)
+				{
+					if(ncs_1.canvas_1.elements[a].type == DragAble) 
+					{
+						for(b in 0...ncs_1.canvas_1.assets.length)
+						{
+							if(ncs_1.canvas_1.assets[b].name == ncs_1.canvas_1.elements[a].asset)
+							{
+								nameToDrop = ncs_1.canvas_1.assets[b].file.substring(13, 14).toUpperCase()
+								+ ncs_1.canvas_1.assets[b].file.substring(14, ncs_1.canvas_1.assets[b].file.length - 4);
+								ncs_1.canvas_1.elements.remove(ncs_1.canvas_1.elements[a]);
+							}
+						}
+						break;
+					}
+					a--;
+				}
+			}
+
+			var currentDir = new Vec4(0.0);
+			var currentLoc = new Vec4(0.0);
+	
+			currentLoc.x = object.transform.worldx();
+			currentLoc.y = object.transform.worldy();
+			currentLoc.z = object.transform.worldz();
+	
+			// get look vector (3m long):
+			currentDir = object.transform.up();
+			currentDir.mult(-3);
+	
+			currentDir.add(currentLoc);
+	
+			iron.Scene.active.spawnObject(nameToDrop, null, function(o:Object)
+			{
+				o.transform.loc = currentDir.add(new Vec4(0,0,1));
+				o.transform.buildMatrix();
 							
 				var dropped = o.getTrait(RigidBody);
 				dropped.syncTransform();
