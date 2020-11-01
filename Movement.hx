b39683f69a0d4f9d7f9e351ee1ead9711099e6c9
LetsCrashGerman <70725566+LetsCrashGerman@users.noreply.github.com>
2020-11-01 19:48:18 +0100

    Add files via upload

--- Movement.hx
+++ Movement.hx
@@ -1,21 +1,36 @@
 package arm;
 
+import bullet.Bt.WheelInfoConstructionInfo;
+import js.html.LabelElement;
+import armory.trait.PhysicsBreak;
 import iron.Trait;
 import iron.Scene;
 import iron.system.Input;
 import iron.math.Vec4;
-import iron.object.Transform;
 import armory.trait.physics.bullet.RigidBody;
 import armory.trait.physics.bullet.PhysicsWorld;
-import armory.trait.internal.CanvasScript;
-import armory.system.Event;
 
 class Movement extends Trait
 {
 	var body: RigidBody;
 	var rotationSpeed = 1;
 	var speed = 0.1;
-	var ground_probe = new Vec4(0, 0, 0); // position of the end of the vector that checks if player touching the ground
+	var ground_b = new Vec4(0, 0, 0);
+	var ground_f = new Vec4(0, 0, 0);
+	var ground_r = new Vec4(0, 0, 0);
+	var ground_l = new Vec4(0, 0, 0); // position of the end of the vector that checks if player touching the ground
+	var rayf : Hit;
+	var rayb : Hit;
+	var rayl : Hit;
+	var rayr : Hit;
+
+	var l:String;
+	var r:String;
+	var f:String;
+	var b:String;
+
+	var a : Int = 0;
+
 	var current_dir = new Vec4(0, 0, 0);
 	var moveLegal: Bool = true;
 
@@ -30,6 +45,7 @@ class Movement extends Trait
 
 		notifyOnUpdate (function()
 		{
+			if (Input.getKeyboard().started("c")) crouching();
 			walking();
 			jumping();
 			look_horizontally();
@@ -44,12 +60,12 @@ class Movement extends Trait
 
 	function walking()
 	{
-		if((PhysicsWorld.active.rayCast(object.transform.loc, ground_probe) != null) && (PhysicsWorld.active.getContacts(body) != null)) // change direction only when touching the ground
+		if(PhysicsWorld.active.getContacts(object.getTrait(RigidBody)) != null) // change direction only when touching the ground
 		{
 			current_dir.set(0, 0, 0);
 
-			if (Input.getKeyboard().down("shift")) speed = 0.2;
-			else speed = 0.1;
+			if ((Input.getKeyboard().down("shift")) && (a != 1)) speed = 0.1;
+			else if (a != 1) speed = 0.05;
 
 			if (Input.getKeyboard().down("w"))
 			{
@@ -73,20 +89,78 @@ class Movement extends Trait
 		
 		body.setAngularFactor(0, 0, 0);
 		
-		var btvec = body.getLinearVelocity();
-		body.setLinearVelocity(0.0, 0.0, btvec.z);
+		//var btvec = body.getLinearVelocity();
+		//body.setLinearVelocity(0.0, 0.0, btvec.z);
 	}
 
 	function jumping()
-	{
-		ground_probe.x = object.transform.loc.x; // take coordinates from the middle of the player
-		ground_probe.y = object.transform.loc.y;
-		ground_probe.z = object.transform.loc.z - 1.3; // distance from middle of body to ground probe idk
+	{	
+		ground_f.x = object.transform.loc.x;
+		ground_f.y = object.transform.loc.y + 0.8;
+		ground_f.z = object.transform.loc.z; //distance from middle of body to ground P
 
-		 // test if there is object between the coordinates:
-		if (Input.getKeyboard().started("space") && (PhysicsWorld.active.rayCast(object.transform.loc, ground_probe) != null))
+		ground_b.x = object.transform.loc.x; 
+		ground_b.y = object.transform.loc.y - 0.8;
+		ground_b.z = object.transform.loc.z; //distance from middle of body to ground P
+
+		ground_l.x = object.transform.loc.x - 0.8; 
+		ground_l.y = object.transform.loc.y;
+		ground_l.z = object.transform.loc.z; // distance from middle of body to ground probe idk
+	
+		ground_r.x = object.transform.loc.x + 0.8; 
+		ground_r.y = object.transform.loc.y;
+		ground_r.z = object.transform.loc.z; // distance from middle of body to ground probe idk		
+		
+		// test if there is object between the coordinates:
+		var rayf = PhysicsWorld.active.rayCast(object.transform.loc, ground_f);
+		var rayb = PhysicsWorld.active.rayCast(object.transform.loc, ground_b);
+		var rayl = PhysicsWorld.active.rayCast(object.transform.loc, ground_l);
+		var rayr = PhysicsWorld.active.rayCast(object.transform.loc, ground_r);
+		
+		 //(PhysicsWorld.active.rayCast(object.transform.loc, ground_probe) != null)
+		if (Input.getKeyboard().started("space") && ((rayf == null) && (rayb == null) && (rayr == null) && (rayl == null)) && PhysicsWorld.active.getContacts(body)[0] != null)
 		{
-			body.applyImpulse(new Vec4(0, 0, 5.25));
+			body.applyImpulse(new Vec4(0, 0, 5));
+		}
+
+		if (Input.getKeyboard().started("b"))
+		{
+			if (rayl != null) l = "true-l";
+			if (rayr != null) r = "true-r";
+			if (rayf != null) f = "true-f";
+			if (rayb != null) b = "true-b";
+
+			//if (PhysicsWorld.active.getContacts(body)[0] != null ) trace(PhysicsWorld.active.getContacts(body));
+			trace (l);
+			trace (r);
+			trace (f);
+			trace (b);
+			trace ("------");
+		}
+
+		l = "false-l";
+		r = "false-r";
+		f = "false-f";
+		b = "false-b";
+	}
+
+	function crouching()
+	{
+		switch (a) 
+		{
+			case 0: 
+				speed = 0.025;
+				a = 1;
+				
+				//object.transform.move(new Vec4(0,0,-0.2)))
+				object.transform.scale.set(0.8,0.8,0.8);
+				//body.transform.buildMatrix();
+
+			case 1:
+				a = 0;
+				//body.btshape.setLocalScaling(new Vector3(1, 1, 1));
+				object.transform.scale.set(1,1,1);
+				//object.transform.move(new Vec4(0,0,0.5));
 		}
 	}
 }
