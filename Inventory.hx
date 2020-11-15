25878a2c29b047c939a63ba0789afc013ed47c81
mrbl4ckscr33n <70725646+mrbl4ckscr33n@users.noreply.github.com>
2020-11-16 00:16:43 +0100

    Add files via upload

--- Inventory.hx
+++ Inventory.hx
@@ -13,42 +13,55 @@ import iron.system.Input;
 import iron.data.Data;
 import zui.NewCanvasScript;
 import zui.ItemOps;
+import armory.data.Config;
+import armory.renderpath.RenderPathCreator; 
 
-class Inventory extends Sight
+class Inventory extends iron.Trait
 {
-	var inventoryOpen: Bool = false;
 	var pickUpHit: Hit;
 	var pickedUp: Bool = false;
-	//var itemContainer = new Array<RigidBody>();
 	var ncs_1: NewCanvasScript;
+	// inventory:
 	var background_1: Telement = {id: null, type: Container, name: "background", x: 700, y: 200, width: 100, height: 100, alignment: 5, visible: false};
 	var itemDescription: Telement = {id: null, type: Text, name: "itemDescription", x: 0, y: 0, width: 150, height: 25, visible: false, color_text: 0xffffffff, text: "this is your inventory."};
 	var descriptionBox: Telement = {id: null, type: FRectangle, name: "descriptionBox", x: 0, y: 0, width: 150, height: 25, visible: false, color: 0xff000000};
 	var select: Telement = {id: null, type: Image, name: "select", x: 700, y: 200, width: 100, height: 100, visible: false};
 	var asset_1: Tasset = {id: null, file: "../../Assets/inventory_background.png", name: null};
 	var asset_2: Tasset = {id: null, file: "../../Assets/select.png", name: null};
-
+	// escape_menu:
+	var option_1: Telement = {id: null, type: Combo, name: "kacktangle", x: 0, y: 0, width: 150, height: 25, visible: false, color: 0xff000000, color_text: 0xffff0000, color_progress: 0xffffff00, color_hover: 0xffffa500, text: "hallo;Fettsack!;Pimmelbirne"};
+	
 	public function new()
 	{
 		super();
 
 		ncs_1 = new NewCanvasScript();
-		ItemOps.pushElements(ncs_1, background_1, asset_1);
-		ItemOps.pushElements(ncs_1, itemDescription);
-		ItemOps.pushElements(ncs_1, select, asset_2);
-		ItemOps.pushElements(ncs_1, descriptionBox);
+		ncs_1.canvas_array[0] = {name: "inventory", x: 0, y: 0, width: 1920, height: 1080, elements: null, theme: "Default Light", assets: null};
+		ncs_1.canvas_array[1] = {name: "menu", x: 0, y: 0, width: 1920, height: 1080, elements: null, theme: "Default Light", assets: null};
+		ItemOps.pushElements(ncs_1, ncs_1.canvas_array[0], background_1, asset_1);
+		ItemOps.pushElements(ncs_1, ncs_1.canvas_array[0], itemDescription);
+		ItemOps.pushElements(ncs_1, ncs_1.canvas_array[0], select, asset_2);
+		ItemOps.pushElements(ncs_1, ncs_1.canvas_array[0], descriptionBox);
+		ItemOps.pushElements(ncs_1, ncs_1.canvas_array[1], option_1);
 
 		notifyOnInit(function()
 		{
-
+			Input.getMouse().lock();
 		});
 
 		notifyOnUpdate(function()
 		{
-			if(Input.getKeyboard().started("tab")) toggleInventory();
-			lockMouse();
+			if(Input.getKeyboard().started("tab")) toggleVisibility(ncs_1.canvas_array[0]);
+			if(Input.getKeyboard().started("escape")) toggleVisibility(ncs_1.canvas_array[1]);
+			if(Input.getKeyboard().started("y"))
+			{
+				Config.raw.rp_ssr = !Config.raw.rp_ssr;
+				RenderPathCreator.applyConfig();
+				Config.save();
+			}
+			
 			placeDown();
-			if(inventoryOpen)
+			if(ncs_1.canvas_array[0].elements[0].visible)
 			{
 				displayDescription();
 			}
@@ -58,42 +71,40 @@ class Inventory extends Sight
 			}
 		});
 
-		Event.add("btn_1_pressed", toggleInventory);
-
 		// notifyOnRemove(function() {
 		// });
 	}
 
 	function displayDescription()
 	{
-		ncs_1.canvas_1.elements.remove(select);
-		ncs_1.canvas_1.elements.push(select);
+		ncs_1.canvas_array[0].elements.remove(select);
+		ncs_1.canvas_array[0].elements.push(select);
 
-		for(a in 0...ncs_1.canvas_1.elements.length)
+		for(a in 0...ncs_1.canvas_array[0].elements.length)
 		{
-			if(ncs_1.canvas_1.elements[a].drag && a < ncs_1.canvas_1.elements.length - 1)
+			if(ncs_1.canvas_array[0].elements[a].drag && a < ncs_1.canvas_array[0].elements.length - 1)
 			{
-				var remove: Telement = ncs_1.canvas_1.elements[a];
-				ncs_1.canvas_1.elements.remove(remove);
-				ncs_1.canvas_1.elements.push(remove);
+				var remove: Telement = ncs_1.canvas_array[0].elements[a];
+				ncs_1.canvas_array[0].elements.remove(remove);
+				ncs_1.canvas_array[0].elements.push(remove);
 			}
 		}
 
-		ncs_1.canvas_1.elements.remove(itemDescription);
-		ncs_1.canvas_1.elements.remove(descriptionBox);
+		ncs_1.canvas_array[0].elements.remove(itemDescription);
+		ncs_1.canvas_array[0].elements.remove(descriptionBox);
 
 		if(Input.getMouse().down() || Input.getMouse().down("right"))
 		{
-			/*for(a in 0...ncs_1.canvas_1.elements.length)
+			/*for(a in 0...ncs_1.canvas_array[0].elements.length)
 			{
-				if(ncs_1.canvas_1.elements[a].type == DragAble)
+				if(ncs_1.canvas_array[0].elements[a].type == DragAble)
 				{
-					for(b in 0...ncs_1.canvas_1.elements.length)
+					for(b in 0...ncs_1.canvas_array[0].elements.length)
 					{
-						if(ncs_1.canvas_1.elements[b].name == "select")
+						if(ncs_1.canvas_array[0].elements[b].name == "select")
 						{
-							ncs_1.canvas_1.elements[b].x = ncs_1.canvas_1.elements[a].x;
-							ncs_1.canvas_1.elements[b].y = ncs_1.canvas_1.elements[a].y;
+							ncs_1.canvas_array[0].elements[b].x = ncs_1.canvas_array[0].elements[a].x;
+							ncs_1.canvas_array[0].elements[b].y = ncs_1.canvas_array[0].elements[a].y;
 						}
 					}
 				}
@@ -102,32 +113,32 @@ class Inventory extends Sight
 		}
 		else
 		{
-			ncs_1.canvas_1.elements.push(descriptionBox);
-			ncs_1.canvas_1.elements.push(itemDescription);
+			ncs_1.canvas_array[0].elements.push(descriptionBox);
+			ncs_1.canvas_array[0].elements.push(itemDescription);
 		}
 
 		var mouseX = Input.getMouse().lastX;
 		var mouseY = Input.getMouse().lastY;
 		var hover = false;
 
-		for(a in 0...ncs_1.canvas_1.elements.length)
+		for(a in 0...ncs_1.canvas_array[0].elements.length)
 		{
-			if(ncs_1.canvas_1.elements[a].type == DragAble)
+			if(ncs_1.canvas_array[0].elements[a].type == DragAble)
 			{
-				if ((mouseX >= ncs_1.canvas_1.elements[a].x) && (mouseX <= (ncs_1.canvas_1.elements[a].x + ncs_1.canvas_1.elements[a].width)) && (mouseY >= ncs_1.canvas_1.elements[a].y) && (mouseY <= (ncs_1.canvas_1.elements[a].y + ncs_1.canvas_1.elements[a].height)))
+				if ((mouseX >= ncs_1.canvas_array[0].elements[a].x) && (mouseX <= (ncs_1.canvas_array[0].elements[a].x + ncs_1.canvas_array[0].elements[a].width)) && (mouseY >= ncs_1.canvas_array[0].elements[a].y) && (mouseY <= (ncs_1.canvas_array[0].elements[a].y + ncs_1.canvas_array[0].elements[a].height)))
 				{
 					hover = true;
-					for(b in 0...ncs_1.canvas_1.assets.length)
+					for(b in 0...ncs_1.canvas_array[0].assets.length)
 					{
-						if(ncs_1.canvas_1.assets[b].name == ncs_1.canvas_1.elements[a].asset)
+						if(ncs_1.canvas_array[0].assets[b].name == ncs_1.canvas_array[0].elements[a].asset)
 						{
-							ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 1].text = ncs_1.canvas_1.assets[b].file.substring(13, 14)
-							+ ncs_1.canvas_1.assets[b].file.substring(14, ncs_1.canvas_1.assets[b].file.length - 4);
+							ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 1].text = ncs_1.canvas_array[0].assets[b].file.substring(13, 14)
+							+ ncs_1.canvas_array[0].assets[b].file.substring(14, ncs_1.canvas_array[0].assets[b].file.length - 4);
 							hover = true;
-							ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 1].x = mouseX;
-							ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 1].y = mouseY;
-							ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 2].x = mouseX + 10;
-							ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 2].y = mouseY + 14;
+							ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 1].x = mouseX;
+							ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 1].y = mouseY;
+							ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 2].x = mouseX + 10;
+							ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 2].y = mouseY + 14;
 							break;
 						}
 					}
@@ -135,39 +146,33 @@ class Inventory extends Sight
 				}
 			}
 		}
-		if(!hover) //ncs_1.canvas_1.elements[ncs_1.canvas_1.elements.length - 1].text = "";
+		if(!hover) //ncs_1.canvas_array[0].elements[ncs_1.canvas_array[0].elements.length - 1].text = "";
 		{
-			ncs_1.canvas_1.elements.remove(itemDescription);
-			ncs_1.canvas_1.elements.remove(descriptionBox);
+			ncs_1.canvas_array[0].elements.remove(itemDescription);
+			ncs_1.canvas_array[0].elements.remove(descriptionBox);
 		}
 	}
 
-	function toggleInventory()
+	function toggleVisibility(canvas_1: Tcanvas)
 	{
 		if (Input.occupied) return;
-			
-		if(Input.getMouse().locked)
-		{
-			ncs_1.setCanvasVisibility(true);
-			inventoryOpen = true;
-			Input.getMouse().unlock(); // unlock mouse while in inventory
-		}
 
-		else if(!Input.getMouse().locked)
+		if(canvas_1.elements[0].visible)
 		{
-			ncs_1.setCanvasVisibility(false);
-			inventoryOpen = false;
-			Input.getMouse().lock(); // lock mouse when inventory is closed
+			ncs_1.setCanvasVisibility(canvas_1, false);
+			var visible: Bool = false;
+			for(a in 0...ncs_1.canvas_array.length)
+			{
+				if(ncs_1.canvas_array[a].elements[0].visible) visible = true;
+			}
+			if(!visible) Input.getMouse().lock();
+		}
+		else
+		{
+			ncs_1.setCanvasVisibility(canvas_1, true);
+			Input.getMouse().unlock();
 		}
 	};
-
-	function lockMouse()
-	{
-		if (Input.occupied) return;
-			
-		if (Input.getMouse().started() && !Input.getMouse().locked && !inventoryOpen) Input.getMouse().lock();
-		else if (Input.getKeyboard().started("escape") && Input.getMouse().locked) Input.getMouse().unlock();
-	}
 
 	function pickUp()
 	{
@@ -196,7 +201,7 @@ class Inventory extends Sight
 			{
 				if(pickUpHit.rb.object.properties["pickUpAble"])
 				{
-					if(ItemOps.pushItems(ncs_1, pickUpHit.rb)) pickUpHit.rb.object.remove();
+					if(ItemOps.pushItems(ncs_1, ncs_1.canvas_array[0], pickUpHit.rb)) pickUpHit.rb.object.remove();
 				}
 			}
 			pickedUp = false;
@@ -209,20 +214,20 @@ class Inventory extends Sight
 		{
 			//find first DragAble in canvas:
 			var nameToDrop: String = null;
-			if(ncs_1.canvas_1.elements != null)
+			if(ncs_1.canvas_array[0].elements != null)
 			{
-				var a: Int = ncs_1.canvas_1.elements.length - 1;
+				var a: Int = ncs_1.canvas_array[0].elements.length - 1;
 				while(a >= 0)
 				{
-					if(ncs_1.canvas_1.elements[a].type == DragAble)
+					if(ncs_1.canvas_array[0].elements[a].type == DragAble)
 					{
-						for(b in 0...ncs_1.canvas_1.assets.length)
+						for(b in 0...ncs_1.canvas_array[0].assets.length)
 						{
-							if(ncs_1.canvas_1.assets[b].name == ncs_1.canvas_1.elements[a].asset)
+							if(ncs_1.canvas_array[0].assets[b].name == ncs_1.canvas_array[0].elements[a].asset)
 							{
-								nameToDrop = ncs_1.canvas_1.assets[b].file.substring(13, 14).toUpperCase()
-								+ ncs_1.canvas_1.assets[b].file.substring(14, ncs_1.canvas_1.assets[b].file.length - 4);
-								ncs_1.canvas_1.elements.remove(ncs_1.canvas_1.elements[a]);
+								nameToDrop = ncs_1.canvas_array[0].assets[b].file.substring(13, 14).toUpperCase()
+								+ ncs_1.canvas_array[0].assets[b].file.substring(14, ncs_1.canvas_array[0].assets[b].file.length - 4);
+								ncs_1.canvas_array[0].elements.remove(ncs_1.canvas_array[0].elements[a]);
 								a = -1;
 
 								var currentDir = new Vec4(0.0);
